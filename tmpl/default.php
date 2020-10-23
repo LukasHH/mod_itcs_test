3b140e9f7dcf548d8e172a789a5d90c6448184f6
LukasHH <31522294+LukasHH@users.noreply.github.com>
2020-10-23 21:17:00 +0200

    init

---
+++ tmpl/default.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_custom
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+
+?>
+
+<div class="module" style="background-color: #f9f9f9; border: 1px solid #a1a1a1;">
+	<div class="module_header">
+		<h3 class="">Test Modul</h3>
+		<span class="hairline"></span>
+	</div>
+	<div class="module_content">
+		<?php echo $itcs_license; ?>
+	</div>
+</div>
